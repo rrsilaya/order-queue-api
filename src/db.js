4cca8fd4e5d2ec592fe550fdcbaca16764f423c9
rrsilaya <rrsilaya@up.edu.ph>
2018-03-04 22:14:48 +0800

    Deploy mongodb

--- src/db.js
+++ src/db.js
@@ -2,7 +2,7 @@ import mongoose from 'mongoose';
 
 mongoose.Promise = global.Promise;
 
-const db = mongoose.createConnection('mongodb://localhost/orderingApp');
+const db = mongoose.createConnection('mongodb://admin:admin@ds255768.mlab.com:55768/orderapp');
 
 db.on('error', err => console.log(err.message));
 db.once('open', () => console.log('Successfully connected to database'));
